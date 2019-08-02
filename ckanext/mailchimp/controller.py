dea66848a6ecff3269dbca5cb160d4015390f10b
Bart Saelen <bart.saelen@geosolutions.be>
2019-08-02 17:55:46 +0200

    do some basic validation on the mail address

--- ckanext/mailchimp/controller.py
+++ ckanext/mailchimp/controller.py
@@ -1,18 +1,24 @@
-import ckan.lib.base as base
 from ckan.common import request
+from ckan.controllers.home import HomeController
+from ckan.lib.helpers import flash_success, flash_error
 
 from ckanext.mailchimp.logic.action.create import mailchimp_add_subscriber
 from ckanext.mailchimp.util import name_from_email
 
+from validate_email import validate_email
 
-class NewsletterController(base.BaseController):
+
+class NewsletterController(HomeController):
 
     def subscribe(self):
         email = request.params.get('email', None)
-        if email:
+        if email and validate_email(email):
             names = name_from_email(email)
             success = mailchimp_add_subscriber(names[0], names[1], email)
-            # TODO handle success or failure
+            if success:
+                flash_success("Successfully added to mailing list")
+            else:
+                flash_error("An error occurred while adding you to the mailing list")
         else:
-            # TODO handle no email
-            pass
+            flash_error("Please provide a valid email address!")
+        return super(NewsletterController, self).index()
