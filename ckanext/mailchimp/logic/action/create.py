47896bddb6c500a9bcb6f8db33105d7c12773e87
Ruben <ruben.de.facq@redpencil.io>
2025-10-16 10:52:31 +0200

    fix subscribe for new users
    Use new add_subscriber logic.
    This means correctly determining if a user is already subscribed and handling any state correctly.
    
    Note that this code is still not perfect, as it has some custom functionality for subscribing as a non-user, but it is also just named "add_subscriber".
    This needs a small refactor to be more clear about the distinction.

--- ckanext/mailchimp/logic/action/create.py
+++ ckanext/mailchimp/logic/action/create.py
@@ -8,32 +8,30 @@ def mailchimp_user_create(context, data_dict):
     user = user_create(context, data_dict)
 
     if user and (data_dict or {}).get('newsletter') == 'subscribed':
-        first, last = name_splitter(data_dict.get('fullname', data_dict.get('name')))
-        mailchimp_add_subscriber(first, last, data_dict.get('email'), tags=["NAP-user"])
+        name = data_dict.get('fullname', data_dict.get('name'))
+        mailchimp_add_subscriber(name, data_dict.get('email'), tags=["NAP-user"])
     return user
 
 
-def mailchimp_add_subscriber(firstname, lastname, email, tags=None):
+def mailchimp_add_subscriber(name, email, tags=None):
     """
     if user is not already in mailchimp add user to mailchimp
 
-    :param firstname: first name of the subscriber
-    :param lastname: last name of the subscriber
+    :param name: full name of the subscriber
     :param email: email of the subscriber
     :param tags: array of tags for the subscriber -> https://mailchimp.com/help/manage-tags/
     :return: True if successful, False if not
     """
     mailchimp_client = get_mailchimp_client()
-    subscriber = mailchimp_client.find_subscriber_by_email(email)
-    if not subscriber:
+    if not mailchimp_client.is_active_subscriber(email):
         success, message = mailchimp_client.create_new_subscriber(
-            firstname,
-            lastname,
+            name,
             email,
             tags
         )
         return success, message
     else:
+        subscriber = mailchimp_client.find_subscriber_by_email(email)
         subscriber_tags = [tag.get('name', '') for tag in subscriber.get('tags', [])]
         merged_tags = subscriber_tags + tags if tags else subscriber_tags
         success = mailchimp_client.update_subscriber_tags(subscriber.get('id'), merged_tags)
