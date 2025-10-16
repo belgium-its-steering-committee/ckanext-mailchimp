592128274f2622d96d16c31f5e79aba8cfa7cf99
Ruben <ruben.de.facq@redpencil.io>
2025-10-16 11:58:47 +0200

    show flash message in correct language
    As this was a global constant, it did not recalculate depending on the language of the user. So move it inside the function.

--- ckanext/mailchimp/controller.py
+++ ckanext/mailchimp/controller.py
@@ -6,16 +6,18 @@ from ckan.lib.helpers import flash_success, flash_error
 from ckanext.mailchimp.logic.action.create import mailchimp_add_subscriber
 from ckanext.mailchimp.util import name_from_email
 
-FLASH_MESSAGES = {
-    "SUCCESS": tk._("A confirmation email was sent to you to verify your email address. Please check your spam folder if you did not receive this email. If the problem persists, contact us at <a href='mailto:contact@transportdata.be'>contact@transportdata.be</a>"),
-    "ERROR_ADD": tk._("An error occurred while adding you to the mailing list."),
-    "ALREADY_SUBSCRIBED": tk._("You already subscribed to the newsletter."),
-    "ERROR_UPDATE": tk._("An error occurred while updating your information."),
-    "ERROR_NOT_VALID": tk._("Please provide a valid email address!"),
-}
 
 
 def flash_message(msg_key):
+    # do not move outside of function: needs to reget translation on each call
+    # to make sure the correct language is used
+    FLASH_MESSAGES = {
+      "SUCCESS": tk._("A confirmation email was sent to you to verify your email address. Please check your spam folder if you did not receive this email. If the problem persists, contact us at <a href='mailto:contact@transportdata.be'>contact@transportdata.be</a>"),
+      "ERROR_ADD": tk._("An error occurred while adding you to the mailing list."),
+      "ALREADY_SUBSCRIBED": tk._("You already subscribed to the newsletter."),
+      "ERROR_UPDATE": tk._("An error occurred while updating your information."),
+      "ERROR_NOT_VALID": tk._("Please provide a valid email address!"),
+    }
     message = FLASH_MESSAGES.get(msg_key)
     if not message:
         return tk._("An error occurred")
@@ -30,8 +32,8 @@ def subscribe():
     )
 
     if email and not errors:
-        first, last = name_from_email(email)
-        success, msg_key = mailchimp_add_subscriber(first, last, email, tags=["Mailinglist-user"])
+        name = name_from_email(email)
+        success, msg_key = mailchimp_add_subscriber(name, email, tags=["Mailinglist-user"])
         if success:
             flash_success(flash_message(msg_key), allow_html=True)
         else:
